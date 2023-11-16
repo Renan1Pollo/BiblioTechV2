f181773887502cef4771e300aa69cb3f08b717ae
Renan Pollo Benelli <renansk18@outlook.com>
2023-11-15 22:01:21 -0300

    Adição de filtros front-end

---
+++ Migrations/20231115171808_InitialCreateX.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiblioTech_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateX : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
